27531ee35244be5ce700b4d7e02b9188f60339fe
Frankie Riviera <frankgriviera@outlook.com>
2019-11-09 17:42:25 -0500

    Updates

---
+++ precache-manifest.1fa81e242acdd1c7f3f0b14765b08db8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "320d1ae0ee90a94f81ebc5de309fda96",
+    "url": "/static/media/OldFgrWebsite.320d1ae0.JPG"
+  },
+  {
+    "revision": "29368b98c06abf214054",
+    "url": "/static/css/main.ec5d8ea4.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c6a8d877ac7ea4637658f854c4923fd",
+    "url": "/static/media/CityOfDetroit.8c6a8d87.jpg"
+  },
+  {
+    "revision": "87c6c62c3c3198740dc3",
+    "url": "/static/js/2.2fc0f1be.chunk.js"
+  },
+  {
+    "revision": "60ddb9c7194d674db45ef0df21b426cc",
+    "url": "/static/media/FGR_API_Transparent.60ddb9c7.png"
+  },
+  {
+    "revision": "a3e0ff0935f24befd1d65c11f9eba050",
+    "url": "/static/media/Square_large.a3e0ff09.jpg"
+  },
+  {
+    "revision": "29368b98c06abf214054",
+    "url": "/static/js/main.08deebe9.chunk.js"
+  },
+  {
+    "revision": "fa87763737ff382f20a0ce2132ba4228",
+    "url": "/static/media/geolocation.fa877637.png"
+  },
+  {
+    "revision": "d58346087cce6a3c9d9ebad0f6ad8b01",
+    "url": "/static/media/GigHub.d5834608.jpg"
+  },
+  {
+    "revision": "d29447dca5322c1343ec85dfb56ffc4b",
+    "url": "/static/media/fgrapiviewerSquare.d29447dc.png"
+  },
+  {
+    "revision": "ac447a8b9065171c87566d3e38b783f0",
+    "url": "/static/media/TaskMaster.ac447a8b.png"
+  },
+  {
+    "revision": "87c6c62c3c3198740dc3",
+    "url": "/static/css/2.b12cb83f.chunk.css"
+  },
+  {
+    "revision": "4d061e57edf958af29b1b7453e07bde0",
+    "url": "/index.html"
+  }
+];
