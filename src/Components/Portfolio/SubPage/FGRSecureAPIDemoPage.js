3e11c24ff6f7ae9950d79bf4409a24205ce86b4c
Frankie Riviera <frankgriviera@outlook.com>
2021-06-01 12:30:55 -0400

    added source code links to fgr-secure-app

--- src/Components/Portfolio/SubPage/FGRSecureAPIDemoPage.js
+++ src/Components/Portfolio/SubPage/FGRSecureAPIDemoPage.js
@@ -33,12 +33,12 @@ class FGRSecureAPIDemoPage extends Component {
           </h5>
           <h2>Overview of Microservice Architecture</h2>
           <img className="explaination_image" src={architecturediagram} alt="Graphic of microservice architecture" />
-          <p>A User signs in with an existing Microsoft account, or signs up using an email. This is done using Azure Active Directory B2C User Flows. Once authenticated, 
-            calls are made to backend apis by using a Json Web Token obtained from AAD B2C. API Management uses the validate-jwt policy to allow traffic to continue to backend APIS.
-            Mutual TLS is used to secure communication between API Management and the AKS cluster. 
-            API Management obtains a client certifate from azure key vault in order to communicate with the AKS cluster. 
-            The AKS cluster contains its server certifate in a Kubernetes Secret that is used by the Ingress Resource when traffic first arrives at the Azure Load Balancer.
-            Data is persisted outside the AKS cluster using an Azure Managed Disc.
+          <p>A User signs in with an existing Microsoft account, or signs up using an email. This is done using Azure Active Directory B2C User Flows. Once authenticated,
+          calls are made to backend apis by using a Json Web Token obtained from AAD B2C. API Management uses the validate-jwt policy to allow traffic to continue to backend APIS.
+          Mutual TLS is used to secure communication between API Management and the AKS cluster.
+          API Management obtains a client certifate from azure key vault in order to communicate with the AKS cluster.
+          The AKS cluster contains its server certifate in a Kubernetes Secret that is used by the Ingress Resource when traffic first arrives at the Azure Load Balancer.
+          Data is persisted outside the AKS cluster using an Azure Managed Disc.
           </p>
           <strong>Key Features include...</strong>
           <ul className="keyFeatures">
@@ -49,14 +49,19 @@ class FGRSecureAPIDemoPage extends Component {
           <strong>Highlighted technologies and services involved...</strong>
           <ul className="keyFeatures">
             {fgr_secure_api.technologies.map(technology => {
-              return <> 
-              <span className="technology-used ">{technology.tech} : </span>
-              <span >{technology.use}</span>
-              <br></br>
+              return <>
+                <span className="technology-used ">{technology.tech} : </span>
+                <span >{technology.use}</span>
+                <br></br>
               </>;
             })}
           </ul>
-         
+          
+          <h2>Source Code</h2>
+          <p className="repourl" >FGR Secure API Frontend <a target="_blank" href="https://github.com/DaRiv94/fgr-secure-api">Here</a></p>
+          <p className="repourl">FGR Secure API backend Node APIs <a target="_blank" href="https://github.com/DaRiv94/fgr-secure-node-backend">Here</a></p>
+
+
           <h5>
             Back to <a href="/portfolio">Portfolio Page</a>
           </h5>
