906160525794f3aeb0463306d7a81c4c32887991
Frankie Riviera <frankgriviera@outlook.com>
2024-03-08 17:06:38 -0500

    small change to reccommendations link

--- src/Components/Common/DeviceRecommendations.js
+++ src/Components/Common/DeviceRecommendations.js
@@ -5,7 +5,7 @@ function PrivacyPolicy() {
     return (
         <div className="home container">
             <div className="NotFound-div">
-                <h1>Device Recommendations</h1>
+                <h1>Device Recommendations links</h1>
 
                 <p>Web cam Razer Kiyp : <a href="https://amzn.to/3ODkonZ">https://amzn.to/3ODkonZ</a></p>
                 <p>Monitor stand : <a href="https://amzn.to/3vh1zj6">https://amzn.to/3vh1zj6</a></p>
